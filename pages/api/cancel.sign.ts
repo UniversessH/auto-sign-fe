b0b33824db085439cda880b4cf91dd5a31c3b91d
universess <universess@qq.com>
2022-03-12 14:03:15 +0800

    fix: ignore cancelSign methond

--- pages/api/cancel.sign.ts
+++ pages/api/cancel.sign.ts
@@ -3,5 +3,5 @@ import client from "./util/client";
 export const cancelSign = async (data: any) => {
   let formData = new FormData();
   formData.append("Url", data.url);
-  await client.delete("/report", formData);
+  // await client.delete("/report", formData);
 };
