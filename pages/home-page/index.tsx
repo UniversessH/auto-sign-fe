f3aad999f9979efec3031bb4da42585f95945305
universess <universess@qq.com>
2022-03-12 11:14:24 +0800

    fix: remove antd-mobile

--- pages/home-page/index.tsx
+++ pages/home-page/index.tsx
@@ -2,14 +2,10 @@ import React from "react";
 
 export const HomePage: React.FC = () => {
   return (
-  <div>
-    <div>How to Use</div>
     <div>
-      填写自动打卡信息
+      <div></div>
+      <div>填写自动打卡信息</div>
+      <div></div>
     </div>
-    <div>
-      
-    </div>
-  </div>
   );
 };
