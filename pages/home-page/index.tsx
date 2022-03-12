166859152b79598699ec2408e1bc9d19b77c6230
universess <universess@qq.com>
2022-03-12 11:09:44 +0800

    build: remove antd

---
+++ pages/home-page/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const HomePage: React.FC = () => {
+  return (
+  <div>
+    <div>How to Use</div>
+    <div>
+      填写自动打卡信息
+    </div>
+    <div>
+      
+    </div>
+  </div>
+  );
+};
